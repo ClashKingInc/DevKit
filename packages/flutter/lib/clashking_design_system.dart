cf1b03945f17c5c6d57e5efe56ebd98dc8f884d4
Destinea <valentine-gilles@orange.fr>
2026-07-09 10:44:38 +0200

    feat: add Flutter design token package

--- packages/flutter/lib/clashking_design_system.dart
+++ packages/flutter/lib/clashking_design_system.dart
@@ -1,14 +1,19 @@
 library clashking_design_system;
 
+import 'package:flutter/material.dart';
+
 class CKColors {
   CKColors._();
 
-  static const int primaryRed = 0xFFD90709;
-  static const int secondaryBlue = 0xFF026CC2;
-  static const int warGold = 0xFFE8A524;
-  static const int capitalPurple = 0xFF8D63D9;
-  static const int builderBlue = 0xFF2A9FD6;
-  static const int donationGreen = 0xFF22C55E;
+  static const Color primaryRed = Color(0xFFD90709);
+  static const Color secondaryBlue = Color(0xFF026CC2);
+  static const Color warGold = Color(0xFFE8A524);
+  static const Color capitalPurple = Color(0xFF8D63D9);
+  static const Color builderBlue = Color(0xFF2A9FD6);
+  static const Color donationGreen = Color(0xFF14A37F);
+  static const Color lossRed = Color(0xFFE35D4F);
+  static const Color capitalOrange = Color(0xFFE56B2F);
+  static const Color capitalTrophy = Color(0xFFD8891F);
 }
 
 class CKRadius {
@@ -16,7 +21,17 @@ class CKRadius {
 
   static const double control = 12;
   static const double chip = 16;
+  static const double card = 20;
   static const double panel = 28;
+  static const double pill = 999;
+}
+
+class CKOpacity {
+  CKOpacity._();
+
+  static const double border = 0.28;
+  static const double borderStrong = 0.32;
+  static const double fillMuted = 0.45;
 }
 
 class CKSpacing {
